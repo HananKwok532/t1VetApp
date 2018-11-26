09b39c0d5a0cb5e8583c4cf1b3f0c071dbddc760
hanankwok532 <hanankwok532@gmail.com>
2018-11-26 16:41:47 -0500

    Changes to EditAppointmentActivity.java

--- app/src/main/java/com/example/hkwok1/vetapp/ExistingAppointmentsActivity.java
+++ app/src/main/java/com/example/hkwok1/vetapp/ExistingAppointmentsActivity.java
@@ -1,8 +1,15 @@
 package com.example.hkwok1.vetapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 
 public class ExistingAppointmentsActivity extends AppCompatActivity {
 
@@ -10,5 +17,7 @@ public class ExistingAppointmentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_existing_appointments);
+        
     }
 }
+
