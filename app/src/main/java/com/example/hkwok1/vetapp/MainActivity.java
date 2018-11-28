18d42914182babd68dfd25c3747b7b2954d10400
hanankwok532 <hanankwok532@gmail.com>
2018-11-27 20:23:07 -0500

    Integrated more files from shyampatel13 and lroseler into the Android Studio project

--- app/src/main/java/com/example/hkwok1/vetapp/MainActivity.java
+++ app/src/main/java/com/example/hkwok1/vetapp/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.hkwok1.vetapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // DatabaseHelper helper = new DatabaseHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signUp = findViewById(R.id.suButton);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUp();
+            }
+        });
+
+        Button login = findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClick(v);
+            }
+        });
+    }
+
+    public void onButtonClick(View v){
+        EditText a = findViewById(R.id.username);
+        String str = a.getText().toString();
+        EditText b = findViewById(R.id.password);
+        String pass = b.getText().toString();
+
+        String password = ""; //helper.searchPass(str);
+        if(pass.equals(password)) {
+            Intent i = new Intent(this, HomePageActivity.class);
+            i.putExtra("username", str);
+            startActivity(i);
+        }
+        else {
+            Toast temp = Toast.makeText(MainActivity.this , "Incorrect Email/Password" , Toast.LENGTH_SHORT);
+            temp.show();
+        }
+
+    }
+
+    public void openSignUp(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/hkwok1/vetapp/RegisterActivity.java
+++ app/src/main/java/com/example/hkwok1/vetapp/RegisterActivity.java
@@ -1,13 +1,67 @@
 package com.example.hkwok1.vetapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    //DatabaseHelper helper = new DatabaseHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button back = findViewById(R.id.returnButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+
+        Button submit = findViewById(R.id.createButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSignUpClick(v);
+            }
+        });
     }
+
+    public void openLogin() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSignUpClick(View v) {
+        if(v.getId() == R.id.createButton) {
+            EditText firstName = findViewById(R.id.fnameEntry);
+            EditText lastName = findViewById(R.id.lnameEntry);
+            EditText phone = findViewById(R.id.phoneEntry);
+            EditText email = findViewById(R.id.emailEntry);
+            EditText uName = findViewById(R.id.usernameEntry);
+            EditText pass = findViewById(R.id.passwordEntry);
+
+            String fNameStr = firstName.getText().toString();
+            String lNameStr = lastName.getText().toString();
+            String phoneStr = phone.getText().toString();
+            String emailStr = email.getText().toString();
+            String uNameStr = uName.getText().toString();
+            String passStr = pass.getText().toString();
+
+            /*contact c = new contact();
+            c.setFirstName(fNameStr);
+            c.setLastName(lNameStr);
+            c.setPhone(phoneStr);
+            c.setEmail(emailStr);
+            c.setUsername(uNameStr);
+            c.setPass(passStr);
+            openLogin(); */
+        }
+    }
+
 }
